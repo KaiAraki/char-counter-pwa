8ae1625c9333b79c58b89bca29987405a8f81d18
Kai Araki <39019232+KaiAraki@users.noreply.github.com>
2025-09-25 20:09:52 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'charcounter-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(res =>
+      res || fetch(e.request).then(networkRes => {
+        const copy = networkRes.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return networkRes;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
